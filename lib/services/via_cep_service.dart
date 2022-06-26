4b8488b6ae90034e3c9770432dd785553c25bfb0
joao-lusa <joaosombrio@rede.ulbra.br>
2022-06-26 17:44:10 -0300

    Consultar cep

--- lib/services/via_cep_service.dart
+++ lib/services/via_cep_service.dart
@@ -1,13 +1,19 @@
-
-import 'package:http/http.dart' as http;
 import 'package:web_service/models/result_cep.dart';
+import 'package:dio/dio.dart';
+
+var dio = Dio();
 
 class ViaCepService {
   static Future<ResultCep> fetchCep({String? cep}) async {
     final Uri uri = Uri.parse('https://viacep.com.br/ws/$cep/json/');
-    final response = await http.get(uri);
+    final response = await dio.getUri(uri);
     if (response.statusCode == 200) {
-      return ResultCep.fromJson(response.body);
+      if (response.data.toString()[1] == "e") {
+        throw Exception(
+            'CEP não encontrado, verifique se o CEP informado esta correto');
+      } else {
+        return ResultCep.fromMap(response.data);
+      }
     } else {
       throw Exception('Requisição inválida!');
     }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:another_flushbar/flushbar.dart';
 import 'package:web_service/services/via_cep_service.dart';
+import 'package:share_plus/share_plus.dart';
 
 class HomePage extends StatefulWidget {
   @override
-  _HomePageState  createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   var _searchCepController = TextEditingController();
   bool _loading = false;
+  bool _isCepValid = false;
   bool _enableField = true;
   String? _result;
+  String _shareResult = '';
 
   @override
   void dispose() {
@@ -22,7 +26,18 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Consultar CEP'),
+        title: Text(
+          'Consultar CEP',
+          style: TextStyle(
+              fontWeight: FontWeight.w900, fontSize: 25, letterSpacing: 2),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.share),
+            tooltip: 'Show Snackbar',
+            onPressed: () => _onShare(),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(20.0),
@@ -44,6 +59,7 @@ class _HomePageState extends State<HomePage> {
       keyboardType: TextInputType.number,
       textInputAction: TextInputAction.done,
       decoration: InputDecoration(labelText: 'Cep'),
+      style: const TextStyle(fontSize: 20),
       controller: _searchCepController,
       enabled: _enableField,
     );
@@ -81,20 +97,51 @@ class _HomePageState extends State<HomePage> {
 
     final cep = _searchCepController.text;
 
-    final resultCep = await ViaCepService.fetchCep(cep: cep);
-    print(resultCep.localidade); // Exibindo somente a localidade no terminal
-
-    setState(() {
-      _result = resultCep.toJson();
-    });
-
-    _searching(false);
+    try {
+      if (cep.length == 8) {
+        final resultCep = await ViaCepService.fetchCep(cep: cep);
+        setState(() {
+          _result = resultCep.toJson();
+          _isCepValid = true;
+        });
+        _searching(false);
+      } else {
+        await Flushbar(
+          message: 'O CEP deve conter 8 digitos.',
+          duration: const Duration(seconds: 2),
+        ).show(context);
+        _isCepValid = false;
+        _searching(false);
+      }
+    } catch (e) {
+      setState(() {
+        _buildValidatorFlushbar(e.toString());
+        _result = e.toString();
+        _isCepValid = false;
+      });
+      _searching(false);
+    }
   }
+
+  Future<Widget> _buildValidatorFlushbar(mensagem) async => Flushbar(
+      title: "Erro", message: mensagem, duration: const Duration(seconds: 2));
 
   Widget _buildResultForm() {
     return Container(
-      padding: EdgeInsets.only(top: 20.0),
+      padding: const EdgeInsets.only(top: 20.0),
       child: Text(_result ?? ''),
     );
+  }
+
+  void _onShare() async {
+    if (_isCepValid) {
+      _shareResult = _result!;
+      await Share.share(_shareResult);
+    } else {
+      await Flushbar(
+        message: 'Consulte um CEP antes de compartilhar',
+        duration: const Duration(seconds: 3),
+      ).show(context);
+    }
   }
 }
